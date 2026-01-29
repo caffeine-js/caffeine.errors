72a27e955920cfb9730f11f24fd4b62e1e6b0791
Alan Reis <alanreisanjo@gmail.com>
2026-01-29 12:09:44 -0300

    Esqueci de ajeitar o index.ts

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,4 +1,2 @@
 export { InvalidDomainDataException } from "./invalid-domain-data.exception";
 export { OperationFailedException } from "./operation-failed.exception";
-export { ResourceAlreadyExistsException } from "./resource-already-exists.exception";
-export { ResourceNotFoundException } from "./resource-not-found.exception";
