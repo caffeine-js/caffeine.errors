1ab06c4f5efa782d1822e3f7d79b992b4af754a9
Alan Reis <alanreisanjo@gmail.com>
2026-02-11 23:59:54 -0300

    feat(application): add InvalidOperationException

--- src/application/index.ts
+++ src/application/index.ts
@@ -4,4 +4,5 @@ export { ResourceNotFoundException } from "./resource-not-found.exception";
 export { BadRequestException } from "./bad-request.exception";
 
 export { UnauthorizedException } from "./unauthorized.exception";
+export { InvalidOperationException } from "./invalid-operation.exception";
 export * from "./jwt";
